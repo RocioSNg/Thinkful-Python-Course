27b55edf04e88e6962162f04a5ec3fb3dc67a65b
RocioSNg <rocio.s.ng@gmail.com>
2014-09-25 18:47:56 -0400

    building up Fizz Buzz v2 Program

--- Fizz_Buzz/Fizz_Buzz_v2.py
+++ Fizz_Buzz/Fizz_Buzz_v2.py
@@ -3,5 +3,26 @@
 # Author:      Rocio Ng
 #-------------------------------------------------------------------------------
 
+import sys
+
+#define Fizz Buzz function
 def fizz_buzz(n):
-    print "Fizz buzz counting up to " + str(n)
+    for num in range(1, n+1):
+        if num % 3 == 0 and num % 5 == 0:
+            print "fizz buzz"
+        elif num % 3 == 0:
+            print "fizz"
+        elif num % 5 == 0:
+            print "buzz"
+        else:
+            print num
+
+if len(sys.argv) == 2:
+    print "Fizz buzz counting up to {}" .format(sys.argv[1])
+    fizz_buzz(int(sys.argv[1]))
+
+elif len(sys.argv) == 1:
+    number = raw_input("Please enter a number")
+    #if type(int(number))!= int:
+        #number = raw_input("Please enter a NUMBER")
+    fizz_buzz(int(number))
