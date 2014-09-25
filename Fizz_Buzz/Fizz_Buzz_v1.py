23406b55884713d7edd25cca8182b8d7c8849c2a
RocioSNg <rocio.s.ng@gmail.com>
2014-09-25 17:59:54 -0400

    created new folder for Fizz Buzz Project

---
+++ Fizz_Buzz/Fizz_Buzz_v1.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        Fizz Buzz
+# Author:      Rocio Ng
+#-------------------------------------------------------------------------------
+
+print "Fizz buzz counting up to 100"
+
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print "fizz buzz"
+    elif num % 3 == 0:
+        print "fizz"
+    elif num % 5 == 0:
+        print "buzz"
+    else:
+        print num
