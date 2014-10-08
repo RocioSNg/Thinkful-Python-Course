59368822c3a28a57dc143a836bf2f98155dfc557
RocioSNg <rocio.s.ng@gmail.com>
2014-10-07 23:07:01 -0400

    added extra features to the pirate program

--- Pirate_Bartender/pirate_bartender.py
+++ Pirate_Bartender/pirate_bartender.py
@@ -9,10 +9,14 @@
 import random
 
 def main():
+    print "Ahoy matey! Let me make the perfect drink for ye!!!"
     pirate_questions()
-    pirate_drink()
+    glass = pirate_drink()
+    print "I will make your drink with: "
+    for ingredient in glass:
+        print ingredient
     while True:
-        if raw_input("Would you like to order another drink?").lower() in ('y','yes'):
+        if raw_input("Would you like to order another drink? ").lower() in ('y','yes'):
             pirate_questions()
             pirate_drink()
         else:
@@ -21,11 +25,11 @@ def main():
 #----Dictionaries-----#
 
 questions = {
-    "strong": "Do ye like yer drinks strong?",
-    "salty": "Do ye like it with a salty tang?",
-    "bitter": "Are ye a lubber who likes it bitter?",
-    "sweet": "Would ye like a bit of sweetness with yer poison?",
-    "fruity": "Are ye one for a fruity finish?"
+    "strong": "Do ye like yer drinks strong? ",
+    "salty": "Do ye like it with a salty tang? ",
+    "bitter": "Arrrr ye a lubber who likes it bitter? ",
+    "sweet": "Would ye like a bit of sweetness with yer poison? ",
+    "fruity": "Arrrrr ye one for a fruity finish? "
             }
 
 ingredients = {
@@ -38,13 +42,14 @@ ingredients = {
 
 answers = {"strong": False, "salty": False, "bitter": False, "sweet":False, "fruity" : False}
 
+#--------------Functions----------#
+
 def pirate_questions():
     ''' Asks question corresponding to each drink type(key) in the questions dictionary
         and gather repsonses in the answers dictionary '''
     for key in questions:
         if raw_input(questions[key]).lower() in ('y','yes'):
             answers[key] = True
-    #print answers
     return answers
 
 def pirate_drink():
@@ -54,9 +59,8 @@ def pirate_drink():
     for key in answers:
         if answers[key] == True:
             arr_drink += [random.choice(ingredients[key])]
-    print arr_drink
-    #if len(arr_drink) == 0:
-       # print "I can't make ye a drink without no ingredients!"
+    if len(arr_drink) == 0:
+        print "I can't make ye a drink without no ingredients!"
     return arr_drink
 
 if __name__ == '__main__':
