ab4fce8f552fb39fff4bd485b23315006495a521
rsng84 <rocio.ng@gmail.com>
2014-10-27 18:07:43 -0400

    adding more features to program

--- Bicycle_OOP/bicycle_OOP.py
+++ Bicycle_OOP/bicycle_OOP.py
@@ -9,14 +9,55 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
+import sys
+sys.path.append("c:Dropbox\\Courses_Seminars\\Thinkful_Python_Course\\Bicycle_OOP")
+
 import bicycle_OOP_classes
 
-bike1 =
 
-Bike_Shop1 = Bike_Shop("On Two Wheels", )
 
-def main():
-    pass
+#------------------------Universal bike catalog----------------------------------#
+bike_catalog = {"Schwinn":[19,100],"Trek":[18,400],"Specialized": [15,900],"Cannondale":[19,200],"Giant":[18,450],"Fuji":[20,50]}  #models with weights and production costs
+
+bike_shop1_inventory = {"Schwinn": 3, "Trek": 1, "Specialized":4, "Cannondale":1, "Giant":2, "Fuji": 1}
+
+bike_shop1 = Bike_Shop("On Two Wheels", bike_shop1_inventory)
+
+
+
+bike_shop = bike_shop1
+#inventory = Bike_Shop1.bike_inventory(bike_shop1_models)
+#for bike in inventory:
+ #   print bike.model_name
+print bike_shop.bikes
+#print bike_prices
+
+
+print "Welcome to the bicycle shop called: %s" % bike_shop.name
+
+#------Customers-----#
+cust1 = Customer("Finn", 200)
+cust2 = Customer("Jake", 500)
+cust3 = Customer("Marceline", 1000)
+
+customers = [cust1,cust2, cust3]  #List with all the customers
+
+for cust in customers:
+    print "The name of the customer is: %s" % cust.name
+    afforable_bikes = []
+    for bike in bike_shop.bike_prices:
+        if bike_shop.bike_prices[bike] <= cust.budget:
+            afforable_bikes.append(bike)
+    print "The bikes that they can afford are:"
+    for bike in afforable_bikes:
+        print bike
+for model in bike_shop.inventory:
+    print "We currently have %s of the %s bike model" % (bike_shop.inventory[model], model)
+
+
+#for model in bike_shop.inventory:
+ #   print "We currently have %s of the %s bike model" % (bike_shop.inventory[model], model)
+
 
 if __name__ == '__main__':
     main()
--- Bicycle_OOP/bicycle_OOP_classes.py
+++ Bicycle_OOP/bicycle_OOP_classes.py
@@ -3,7 +3,6 @@
 # Purpose:  Define classes for Bicycle Industry Modelling Project
 #
 # Author:      Rocio Ng
-#
 # Created:     16/10/2014
 #-------------------------------------------------------------------------------
 
@@ -14,12 +13,12 @@ class Bicycle(object):
         self.weight = weight  #in pounds
         self.production_cost = production_cost
 
-
 class Bike_Shop(object):
     def __init__(self, name, inventory):
         self.name = name
         self.inventory = inventory    #dictionary of models of bikes and their inventory numbers
-        self.bikes = self.bike_list
+        self.bikes = self.bike_list(inventory)
+        self.bike_prices = self.bike_sell_price(self.bikes)
 
     def bike_list(self,inventory):
         bikes = []
@@ -37,54 +36,14 @@ class Bike_Shop(object):
         bike_store_prices= {}
         for item in bike_list:
             bike_store_prices[item.model_name] = item.production_cost + (item.production_cost*.2)
-        #return bike_store_prices
-        self.bike_prices = bike_store_prices
+        return bike_store_prices
+        #self.bike_prices = bike_store_prices
 
 class Customer(object):
     can_buy_bike = True
     def __init__(self, name, budget):
         self.name = name
         self.budget = budget
-
-bike_catalog = {"Schwinn":[19,100],"Trek":[18,400],"Specialized": [15,900],"Cannondale":[19,200],"Giant":[18,450],"Fuji":[20,50]}  #models with weights and production costs
-#bike_shop1_models = ["Schwinn", "Trek", "Specialized","Cannondale", "Giant","Fuji"]  #List of models that bike shop1 has
-bike_shop1_inventory = {"Shwinn": 3, "Trek": 1, "Specialized":4, "Cannondale":1, "Giant":2, "Fuji": 1}
-
-
-Bike_Shop1 = Bike_Shop("On Two Wheels", bike_shop1_inventory)
-#inventory = Bike_Shop1.bike_inventory(bike_shop1_models)
-#for bike in inventory:
- #   print bike.model_name
-print Bike_Shop1.bikes
-#print bike_prices
-
-
-print "Welcome to the bicycle shop called: %s" % Bike_Shop1.name
-for key in Bike_Shop1.inventory:
-    print "We currently have %s of the %s bike model" % ( Bike_Shop1.inventory[key], key)
-
-
-#------Customers-----#
-cust1 = Customer("Finn", 200)
-cust2 = Customer("Jake", 500)
-cust3 = Customer("Marceline", 1000)
-
-customers = [cust1,cust2, cust3]  #List with all the customers
-
-for cust in customers:
-    print "The name of the customer is: %s" % cust.name
-    afforable_bikes = []
-
-    for bike in bike_prices:
-        if bike_prices[bike] <= cust.budget:
-            afforable_bikes.append(bike)
-    print "The bikes that they can afford are:"
-    for bike in afforable_bikes:
-        print bike
-
-#print "The %  store has the following left in stock:" % Bike_Shop1.name
-#for bike in bike_shop1_inventory:
-   # print " %s of the %s model left" % ( bike_shop1_inventory[bike], bike)
 
 
 
