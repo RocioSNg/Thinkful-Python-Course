705e04788b664ada755adf696df347100df757ae
rsng84 <rocio.ng@gmail.com>
2014-10-29 14:25:26 -0400

    completed snippets program

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -3,23 +3,20 @@ import csv
 import argparse
 import sys
 
-
 #------------Set the log output file and the log level---------#
-
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
-
 
 #-------------Functions----------------------------------------#
 
 def make_parser():
 	''' Construct the command line parser'''
 	logging.info("Constructing parser")
-	description = "Store and retrieve snippets of text"  # defines purpose of the program for the user
+	description = "Store and retrieve snippets of text"  # of the program purpose
 	parser = argparse.ArgumentParser(description = description)
 	
-	subparser = parser.add_subparsers(dest="commands" help="Available commands")
+	subparser = parser.add_subparsers(dest="command", help="Available commands")
 	
-	# Subparser for the put comman
+	#------Subparser for the put command----------#
 	logging.debug("Constructing put subparser")
 	put_parser = subparser.add_parser("put", help="Store a snippet")
 	put_parser.add_argument("name", help="The name of the snippet")
@@ -29,14 +26,13 @@ def make_parser():
 	return parser
 
 def put(name, snippet, filename):
-	""" Store a snippet with an associated name in the CSV
-	"""
+	''' Store a snippet with an associated name in the CSV'''
 
 	logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
 	logging.debug("Opening file")
-	with open(filename, "a") as f:	# 'a' indicates that we want to creat a file if it doesn't exist, or append it otherwise
-		writer = csv.writer(f)	# writer objet will allow for new rows to be added to the CSV file for each created snippet
-		logging.debug("Writing snipet to file")
+	with open(filename, "a") as f:	# 'a' indicates that we want to create a file if it doesn't exist, or append it otherwise
+		writer = csv.writer(f)	# writer object will allow for new rows to be added to the CSV file for each created snippet
+		logging.debug("Writing snippet to file")
 		writer.writerow([name, snippet])	# adds new rows to the file
 	logging.debug("Write successful")
 	return name, snippet #returns a tuple
@@ -47,5 +43,12 @@ def main():
 	parser = make_parser()
 	arguments = parser.parse_args(sys.argv[1:])  # passes all the command line arguments except the first which is the name of our program
 
+	# convert parsed arguments from Namespace to dictionary
+	arguments = vars(arguments)
+	command = arguments.pop("command")
+	if command == "put":
+		name, snippet = put(**arguments)   # unpacks key value pairs into keyword arguments
+		print "Stored {!r} as {!r}". format(snippet, name)
+
 if __name__ == "__main__":
-main()
+	main()
