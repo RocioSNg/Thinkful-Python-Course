7f8fed2423397ca653a5f3c7b6a1c27da0cca9b8
RocioSNg <rocio.s.ng@gmail.com>
2014-11-05 18:46:59 +0000

    debugging app

--- github_languages/github_languages.py
+++ github_languages/github_languages.py
@@ -3,16 +3,33 @@ import operator
 from collections import defaultdict
 import requests
 
+from secret import USERNAME, PASSWORD
+
 def get_repositories(user):
 	'''Retrieve a list of user's repositories'''
 	url = "https://api.github.com/users/{user}/repos".format(user=user)
-	response = requests.get(url)
+	response = requests.get(url, auth=(USERNAME, PASSWORD))
 	return response.json()
+
+def get_languages_dictionaries(repositories):
+  '''
+  Return a list of dictionaries containing the languages used in each
+  repository
+  '''
+  language_dictionaries = []
+  for repository in repositories:
+    url = "https://api.github.co/repos/{owner}/{repo}/languages"
+    url = url.format(owner=respository["owner"]["login"],
+                     repo=repository["name"])
+    response = requests.get(url, auth=(USERNAME, PASSWORD))
+    language_dictionaries.append(response.json())
+  return language_dictionaries
 
 def main():
 	'''Main function'''
 	repositories = get_repositories(sys.argv[1])
-	print repositories
+  language_dictionaries = get_language_dictionaries(respositories)
+	print language_dictionaries
 
 if __name__ == "__main__":
 	main()
