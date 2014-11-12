1b1476e6b070b61a7b05c389be499d9b3515163f
RocioSNg <rocio.s.ng@gmail.com>
2014-11-12 23:11:04 +0000

    added new features to twitter app

--- tweetful/tweetful.py
+++ tweetful/tweetful.py
@@ -7,8 +7,27 @@ from urls import *
 def main():
 	'''Main function'''
 	auth = authorization.authorize()  # authorizes the application
-	response = requests.get(TIMELINE_URL, auth = auth)	
+	
+  	#---Get Home Timeline information for user---#
+  	# Returns a collection of the most recent Tweets and retweets posted by the authenticating user and the users they follow
+    
+  	response = requests.get(TIMELINE_URL, auth = auth)	# uses OAuth1 instance that is returned to authenticate 
 	print json.dumps(response.json(), indent = 4)	# json.dumps formats the output
-
+  
+  	#-- Get folowers of a specified user---#
+  	#Either a screen_name or a user_id should be provided.
+  	screen_name = {"screen_name" : "RocioSNg"}
+  	followers_request = requests.get(FOLLOWERS_URL, params = screen_name, auth = auth)
+  	print json.dumps(followers_request.json(), indent = 4)
+  
+  
+  
+  	#--Post update-----#
+    	status = {"status" : "Hello world"}
+  	post_request = requests.post(POST_REQUEST_URL, params = status, auth = auth)
+    	print json.dumps(post_request.json(), indent = 4)
+  
+  	#sample post request https://api.twitter.com/1.1/statuses/update.json?status=Maybe%20he%27ll%20finally%20find%20his%20keys.%20%23peterfalk
+  
 if __name__ == "__main__":
 	main()
--- tweetful/urls.py
+++ tweetful/urls.py
@@ -1,6 +1,16 @@
 API_URL = "https://api.twitter.com"
+
+#---For authorization of the app-------#
 REQUEST_TOKEN_URL = API_URL + "/oauth/request_token"
 AUTHORIZE_URL = API_URL + "/oauth/authorize?oauth_token={request_token}"
 ACCESS_TOKEN_URL = API_URL + "/oauth/access_token"
+
+#---For accessing the Users home Timeline----#
 TIMELINE_URL = API_URL + "/1.1/statuses/home_timeline.json"
+
+#--For accessing the followers of a specified User
+FOLLOWERS_URL = API_URL + "/1.1/followers/list.json"
+
+#--For posting status updates or tweets------#
+POST_REQUEST_URL = API_URL + "/1.1/statuses/update.json"
 
