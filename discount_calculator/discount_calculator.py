ce93b9d3e2a8eb244b15b678fe6115ebc628acd3
RocioSNg <rocio.s.ng@gmail.com>
2014-11-24 17:58:58 +0000

    minor changes to discount script

--- discount_calculator/discount_calculator.py
+++ discount_calculator/discount_calculator.py
@@ -6,11 +6,13 @@ def calculate_discount(item_cost, relative_discount, absolute_discount):
 	> calculate_discount(200, 10, 30)
 	> 150
 	'''
-  	if relative_discount > 100:
-     		raise ValueError("Discount cannot be above 100")
+  if relative_discount > 100:
+     raise ValueError("Discount cannot be above 100")
       
 	relative_discount_amount = item_cost * (float(relative_discount)/100)
 	final_price = item_cost - relative_discount_amount - absolute_discount
+  
 	if final_price < 0:
 		final_price = 0
+    
 	return final_price
