9cd61a50bb8493883145e6912ae079a1bc962ca0
RocioSNg <rocio.s.ng@gmail.com>
2014-11-26 16:43:02 -0500

    claening folder

--- sqlite_demo/person-demo.sql
+++ sqlite_demo/person-demo.sql
@@ -1,12 +1,12 @@
 drop table if exists person;
+
 create table person (
     id int primary_key not null,
     name text not null,
-    age int 
-),
+    age  int 
+);
 
 insert into person values 
-
     (1, 'Bob', 22),
     (2, 'Sue', 34),
     (3, 'Timmy', 19),
