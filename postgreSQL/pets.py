8c1648fb2dd7609f699550d1efb1c6756d79f856
RocioSNg <rocio.s.ng@gmail.com>
2015-02-05 13:11:41 -0500

    updated and cleaned code

--- postgreSQL/pets.py
+++ postgreSQL/pets.py
@@ -16,8 +16,6 @@ try:
 except:
 	print "I am unable to connect to the database"
 
-# open cursor
-cur = conn.cursor()
 
 #test connection
 cur.execute('''select * from breed''')
@@ -25,80 +23,70 @@ rows = cur.fetchall()
 for row in rows:
 	print "   ", row
 
-cur.execute('''select * from species''')
-rows = cur.fetchall()
-for row in rows:
-	print "   ", row
-
-# field names NEED to be changed to reflect SQL file
+# field names changed for easier updating
 field_names = ['name', 'age', 'breed', 'species', 'shelter', 'adopted']
 
 #----------opens and parses the CSV file, creating a dictionary of items to be inserted into the database
 
 
 # open csv file and convert to dictReader object
-pet_file = open("pets_to_add.csv")
-csv_reader = csv.DictReader(pet_file, delimiter = ',', fieldnames = field_names)
+pets_list = []
+with open("pets_to_add.csv") as pet_file:
+#csv_reader = csv.DictReader(pet_file, delimiter = ',')
+	csv_reader = csv.DictReader(pet_file, delimiter = ',', fieldnames = field_names)
+	next(csv_reader)	# skip header row
+	for row in csv_reader:
+		# row = row.strip()
+		pets_list.append(row)
+
+#csv_reader = (dict((k, v.strip()) for k, v in row.items() if v) for row in reader)
 
 # create list of dictionary items
-pets_list = []
 
-for row in csv_reader:
-	#print row
-	pets_list.append(row)
+print pets_list
 
 # change blanks to none
 pets_to_add_list = []
 for lib in pets_list:
 	l = {k:lib[k]  if lib [k] != ""  else None for k in lib}
 	pets_to_add_list.append(l)
-print pets_to_add_list
+#print pets_to_add_list
 
-
-# check updates
-cur.execute('''select * from pet''')
-rows = cur.fetchall()
-print rows
-#rows = cur.fetchall()
-#for row in rows:
-#	print row
 
 # Create tuple of all data to be added
-pets_to_add =  tuple(pets_to_add_list[1:])	# do not want to include the top row which contain the bad fieldnames
+pets_to_add =  tuple(pets_to_add_list[0:])	
 #print pets_to_add
-
-#print pets_to_add[2]
-#cur.execute("""INSERT INTO pet(name, age, adopted) VALUES (%(name)s,
- 	#%(age)s , %(adopted)s)""", pets_to_add[4])
-cur.execute("""INSERT INTO breed(name) VALUES(%(breed)s,
-	(SELECT id FROM species WHERE name = %(species)s) )""", pets_to_add[0])
-
-
-
-#print pets_to_add[1]
 
 
 #---------Update tables-------#
 
-# add breed to breed table
-#cur.executemany("""INSERT INTO breed(name) VALUES (%(breed)s,
-	#(SELECT id FROM species WHERE name = %(species)s) )""", pets_to_add)
-
-# add Pets 
-cur.executemany("""INSERT INTO pet(name, age, adopted) VALUES (%(name)s,
-	%(age)s , %(adopted)s)""", pets_to_add)
-
-
-# inserts a new row in the pet table for each dict entry
-# missing values as null; normalize for capitalization
+# tests for single entries
+#print pets_to_add[2]
+#cur.execute("""INSERT INTO pet(name, age, adopted) VALUES (%(name)s,
+ 	#%(age)s , %(adopted)s)""", pets_to_add[4])
+#cur.execute("""INSERT INTO breed(name, species_id) VALUES(%(breed)s,
+#	(SELECT id FROM species WHERE name = %(species)s) )""", pets_to_add[0])
 
 
-# test update
+# add shelters
+cur.executemany("""INSERT INTO shelter(name) VALUES (%(shelter)s)""", pets_to_add)
 
+# add breed to breed table.  Get species id from species table
+cur.executemany("""INSERT INTO breed(name, species_id) VALUES (%(breed)s,
+	(SELECT id FROM species WHERE name = %(species)s) )""", pets_to_add)
+
+
+
+# add Pets to pet table.  Get breed ids from breed table
+cur.executemany("""INSERT INTO pet(name, age, adopted, breed_id) VALUES (%(name)s,
+	%(age)s , %(adopted)s, (SELECT id from breed WHERE name = %(breed)s ))""", pets_to_add)
+
+# check updates
 cur.execute('''select * from pet''')
-rows2= cur.fetchall()
-for row in rows2:
-	print row
+rows = cur.fetchall()
+for row in rows:
+	print "   ", row
+
 
 # Commit changes to the database
 conn.commit()
